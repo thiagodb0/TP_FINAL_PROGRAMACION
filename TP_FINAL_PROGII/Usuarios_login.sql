e860e6a1902dc692c5ddfb533bf1c5fdb27b8cc5
thiagodb0 <tdibellasonzini@gmail.com>
2022-11-09 11:42:49 -0300

    Confirmacion

--- TP_FINAL_PROGII/Usuarios_login.sql
+++ TP_FINAL_PROGII/Usuarios_login.sql
@@ -12,3 +12,80 @@ as
 begin 
 select * from Usuarios
 end
+
+-----------Modificacion Producto-------
+create procedure PA_UPD_PROD
+@cod int,
+@desc varchar(200),
+@precio money,
+@cod_tipo int,
+@stock int,
+@stockmin int,
+@model int,
+@cod_marca int
+as 
+begin 
+update productos
+set descripcion = @desc,
+pre_unitario = @precio,
+cod_tipo_prod = @cod_tipo,
+stock = @stock, 
+stock_min = @stockmin,
+cod_modelo = @model,
+cod_marca = @cod_marca
+where cod_producto = @cod
+end
+
+select * from productos
+
+
+
+
+
+
+
+
+
+
+
+-------Carga Cliente---
+select * from clientes
+create procedure PA_INSERT_CLIENTE
+@nombre varchar(100),
+@apellido varchar(100),
+@calle varchar (100),
+@altura int,
+@barrio int,
+@tipoDoc int,
+@tipoCli int ,
+@doc int,
+@telefono bigint,
+@mail varchar(200)
+as
+begin 
+insert into clientes values (@nombre, @apellido, @calle,@altura,@barrio,@tipoDoc,@tipoCli,@doc,@telefono,@mail)
+end
+------Combos Cliente----
+create procedure PA_CONS_TIPODOC
+as
+begin
+select * from tipos_doc
+end
+
+create procedure PA_CONS_BARRIOS_CBA
+as
+begin
+select cod_barrio, barrio
+from barrios
+where cod_localidad = 543
+end
+
+create procedure PA_CONS_TIPO_CLI
+as
+begin 
+select * from tipo_cliente
+end
+
+delete from productos 
+where descripcion like 'prueba%'
+select * from productos
